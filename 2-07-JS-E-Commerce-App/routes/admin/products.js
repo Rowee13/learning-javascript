90fdcebb6f85692e3ff7512e7d6f5d4740c9b174
Rowee Apor <75659500+Rowee13@users.noreply.github.com>
2022-10-10 23:49:38 +0800

    setup image file upload

--- 2-07-JS-E-Commerce-App/routes/admin/products.js
+++ 2-07-JS-E-Commerce-App/routes/admin/products.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const { validationResult } = require("express-validator");
+const multer = require("multer");
+
 const productsRepo = require("../../repositories/products");
 const productsNewTemplate = require("../../views/admin/products/new");
 const { requireTitle, requirePrice } = require("./validators");
 
 const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
 
 router.get("/admin/products", (req, res) => {});
 
@@ -12,11 +15,19 @@ router.get("/admin/products/new", (req, res) => {
 	res.send(productsNewTemplate({}));
 });
 
-router.post("/admin/products/new", [requireTitle, requirePrice], (req, res) => {
-	const errors = validationResult(req);
-	console.log(errors);
+router.post(
+	"/admin/products/new",
+	[requireTitle, requirePrice],
+	upload.single("image"),
+	async (req, res) => {
+		const errors = validationResult(req);
 
-	res.send("submitted");
-});
+		const image = req.file.buffer.toString("base64");
+		const { title, price } = req.body;
+		await productsRepo.create({ title, price, image });
+
+		res.send("submitted");
+	}
+);
 
 module.exports = router;
