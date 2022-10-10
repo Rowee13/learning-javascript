6360173410b308c275ccd1a3f452a1fced1646b9
Rowee Apor <75659500+Rowee13@users.noreply.github.com>
2022-10-10 21:40:52 +0800

    setup product repo and validator

--- 2-07-JS-E-Commerce-App/index.js
+++ 2-07-JS-E-Commerce-App/index.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const cookieSession = require("cookie-session");
 const authRouter = require("./routes/admin/auth");
+const productsRouter = require("./routes/admin/products");
 
 const app = express();
 app.use(express.static("public"));
@@ -13,6 +14,7 @@ app.use(
 	})
 );
 app.use(authRouter);
+app.use(productsRouter);
 
 app.listen(3000, () => {
 	console.log("Listening on port 3000");
