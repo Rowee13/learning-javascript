b293def60f9b748e3b0c6e873ea4ea5df575e2b5
Rowee Apor <75659500+Rowee13@users.noreply.github.com>
2022-10-12 15:58:18 +0800

    tried nodejs assert and mocha

--- 2-08-JS-hidash-test/index.test.js
+++ 2-08-JS-hidash-test/index.test.js
@@ -1,37 +1,31 @@
+const assert = require("assert");
 const { forEach } = require("./index");
 
-const test = (desc, fn) => {
-	console.log("----", desc);
-	try {
-		fn();
-	} catch (err) {
-		console.log(err.message);
-	}
-};
+// const test = (desc, fn) => {
+// 	console.log("----", desc);
+// 	try {
+// 		fn();
+// 	} catch (err) {
+// 		console.log(err.message);
+// 	}
+// };
 
-test("The forEach function", () => {
+it("The forEach function", () => {
 	let sum = 0;
 	forEach([1, 2, 3], (value) => {
 		sum += value;
 	});
 
-	if (sum !== 6) {
-		throw new Error("Expected summing array to equal 6");
-	}
+	assert.strictEqual(sum, 6, "Expected forEach to sum the array");
 });
 
-test("The map function", () => {
+it("The map function", () => {
 	const result = map([1, 2, 3], (value) => {
 		return value * 2;
 	});
 
-	if (result[0] !== 2) {
-		throw new Error(`Expected to find 2, but found ${result[0]}`);
-	}
-	if (result[1] !== 4) {
-		throw new Error(`Expected to find 4, but found ${result[0]}`);
-	}
-	if (result[2] !== 6) {
-		throw new Error(`Expected to find 6, but found ${result[0]}`);
-	}
+	assert.deepStrictEqual(result, [2, 4, 6]);
+	// assert.strictEqual(result[0], 2);
+	// assert.strictEqual(result[1], 4);
+	// assert.strictEqual(result[2], 6);
 });
